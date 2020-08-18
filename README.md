d4bd1207ddd017e174d2b070dc97d90b30b74df8
pipe.zkk <pipe.zkk@alibaba-inc.com>
2020-08-18 10:13:34 +0800

    Update Code

--- README.md
+++ README.md
@@ -1,3 +1,15 @@
 # rollup-plugin-import-to-url
 
 Deeply inspired by `rollup-plugin-esm-import-to-url`.
+
+
+# Options
+
+* domain: ESM loader server
+  * default: jspm.dev
+  * for Alibaba: jspm.alibaba-inc.com
+* npmregistry: Check dependencies' default version
+  * default: registry.npmjs.com
+  * for Alibaba: registry.npm.alibaba-inc.com
+* dependencies: custom dependencies map
+  * default: {}. For example: { qs: '^1.0.0' }
--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 'use strict';
 const path = require('path');
+const fetch = require('node-fetch');
 
-const notBare = (str) =>
+const notBare = str =>
   str.startsWith('/') || str.startsWith('./') || str.startsWith('../');
 
-function getImportVersion(name) {
+/**
+ * Get package's version by name.
+ */
+async function getImportVersion(name, npmregistry) {
+  // Get version by current project's package.json
   const pkg = require(path.resolve(
     __dirname,
     process.env.PWD,
@@ -23,25 +28,57 @@ function getImportVersion(name) {
     return pkg.devDependencies[name];
   }
 
-  return process.env[`npm_package_dependencies_${name.replace(/-/g, '_')}`];
+  // Get version by process env
+  const envVersion =
+    process.env[`npm_package_dependencies_${name.replace(/-/g, '_')}`];
+  if (envVersion) {
+    return envVersion;
+  }
+
+  const remoteInfo = await fetch(`https://${npmregistry}/${name}`).then(d =>
+    d.json()
+  );
+
+  // 需要返回 ^x.x.x，否则会被 simplifyVersion 解析为固定版本
+  return `^${remoteInfo['dist-tags'].latest}`;
 }
 
+/**
+ * Get major version
+ */
 function simplifyVersion(version) {
+  // TODO: 看看有没有更好的处理方式
+  // 目前是先直接暴力去掉所有的非 major 版本号
   if (/^\^/.test(version)) {
     return version.match(/^\^(\d+)/)[1];
+  }
+
+  if (/^~/.test(version)) {
+    return version.match(/^~(\d+)/)[1];
+  }
+
+  if (/^>=/.test(version)) {
+    return version.match(/^>=(\d+)/)[1];
   }
 
   return version;
 }
 
-function importToUrl({ domain = 'jspm.dev' } = {}) {
+function importToUrl({
+  domain = 'jspm.dev',
+  npmregistry = 'registry.npmjs.com',
+  dependencies = {},
+} = {}) {
   return {
     name: 'rollup-plugin-import-to-url',
-    resolveId(importee) {
-      if (!/^http/.test(importee) && !notBare(importee)) {
+    async resolveId(importee) {
+      if (/^http(s?):\/\//i.test(importee)) return null;
+
+      if (!notBare(importee)) {
+        const version = await getImportVersion(importee, npmregistry);
         return {
           id: `https://${domain}/${importee}@${simplifyVersion(
-            getImportVersion(importee)
+            dependencies[importee] || version
           )}`,
           external: true,
         };
